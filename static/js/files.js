a3c4eb600237bbaf0f47faa35e831ea9ad72e5fa
Raihan Muzzammil <raihanmuzzammil@gmail.com>
2021-09-02 17:08:53 +0530

    working build

---
+++ static/js/files.js
@@ -0,0 +1,14 @@
+function filesz() {
+    const fi = document.getElementById('file');
+        if (fi.files.length > 0) {
+            for (let n = 0; n <= fi.files.length - 1; n++) {
+                const fsize = fi.files.item(n).size;
+                const file = Math.round((fsize / 1024));
+                    if (file >= 20000) {
+                    alert("File too Big, please select a file less than 20mb");
+                    document.getElementById('size').innerHTML = '<b>'
+                        + file + '</b> KB';
+                }
+            }
+        }
+    }
